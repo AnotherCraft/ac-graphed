2e0001465e5468d3bafd4e1225a774da0c7d274b
Danol <czdanol@gmail.com>
2021-05-19 18:08:25 +0200

    no message

--- dataflow/src/nodes.js
+++ dataflow/src/nodes.js
@@ -2,10 +2,9 @@
 {
 	{
 		function node() {
-			this.addOutput("", "number");
+			this.addOutput("v", "number");
 			this.addProperty("name", "time");
 			this.widget = this.addWidget("text", "name", "time", "name");
-			this.widgets_up = true;
 		}
 
 		node.title = "Number input";
@@ -19,10 +18,9 @@
 {
 	{
 		function node() {
-			this.addOutput("", "pose");
-			this.addProperty("name", "default");
-			this.widget = this.addWidget("text", "name", "default", "name");
-			this.widgets_up = true;
+			this.addOutput("v", "pose");
+			this.addProperty("name", "pose:default");
+			this.widget = this.addWidget("text", "name", "pose:default", "name");
 		}
 
 		node.title = "Pose input";
@@ -33,10 +31,9 @@
 
 	{
 		function node() {
-			this.addInput("", "pose");
+			this.addInput("v", "pose");
 			this.addProperty("name", "pose");
 			this.widget = this.addWidget("text", "name", "pose", "name");
-			this.widgets_up = true;
 		}
 
 		node.title = "Pose output";
@@ -47,7 +44,7 @@
 
 	{
 		function node() {
-			this.addOutput("", "pose");
+			this.addOutput("v", "pose");
 			this.addInput("a", "pose");
 			this.addInput("b", "pose");
 			this.addInput("t", "number");
