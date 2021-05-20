39581ef9ddd11a46a58eaaab37f551b739db68ad
Danol <czdanol@gmail.com>
2021-05-20 11:07:18 +0200

    no message

--- dataflow/src/nodes.js
+++ dataflow/src/nodes.js
@@ -6,11 +6,27 @@
 			this.addProperty("name", "time");
 			this.widget = this.addWidget("text", "name", "time", "name");
 		}
-
 		node.title = "Number input";
-		node.desc = "";
-
 		LiteGraph.registerNodeType("number/input", node);
+	}
+
+	{
+		function node() {
+			this.addOutput("v", "number");
+			this.addInput("a", "number");
+		}
+		node.title = "Zigzag";
+		node.desc = "Similar to modulo 1, but goes to v=1 at a=0.5 and back to v=0 at a=1 (and repeating)";
+		LiteGraph.registerNodeType("number/zigzag", node);
+	}
+
+	{
+		function node() {
+			this.addOutput("v", "number");
+			this.addInput("a", "number");
+		}
+		node.title = "Smoothstep";
+		LiteGraph.registerNodeType("number/smoothstep", node);
 	}
 }
 
@@ -22,10 +38,7 @@
 			this.addProperty("name", "pose:default");
 			this.widget = this.addWidget("text", "name", "pose:default", "name");
 		}
-
 		node.title = "Pose input";
-		node.desc = "";
-
 		LiteGraph.registerNodeType("pose/input", node);
 	}
 
@@ -35,10 +48,7 @@
 			this.addProperty("name", "pose");
 			this.widget = this.addWidget("text", "name", "pose", "name");
 		}
-
 		node.title = "Pose output";
-		node.desc = "";
-
 		LiteGraph.registerNodeType("pose/output", node);
 	}
 
@@ -49,10 +59,8 @@
 			this.addInput("b", "pose");
 			this.addInput("t", "number");
 		}
-
 		node.title = "Mix poses";
-		node.desc = "";
-
+		node.desc = "Linearly mixes two poses";
 		LiteGraph.registerNodeType("pose/mix", node);
 	}
 
