4a5428d4026a693caa7871da01efc9954f451cc0
Danol <czdanol@gmail.com>
2021-05-20 16:01:59 +0200

    no message

--- dataflow/src/nodes.js
+++ dataflow/src/nodes.js
@@ -1,13 +1,74 @@
+function registerInputNode(type, defaultValue) {
+	function node() {
+		this.addOutput("v", type);
+		this.addProperty("name", defaultValue);
+		this.addWidget("text", "name", defaultValue, "name");
+	}
+	node.title = "Input (" + type + ")";
+	LiteGraph.registerNodeType(type + "/input", node);
+}
+function registerOutputNode(type, defaultValue) {
+	function node() {
+		this.addInput("v", type);
+		this.addProperty("name", defaultValue);
+		this.addWidget("text", "name", defaultValue, "name");
+	}
+	node.title = "Output (" + type + ")";
+	LiteGraph.registerNodeType(type + "/output", node);
+}
+function registerConstantNode(type, defaultValue, widgetType) {
+	function node() {
+		this.addOutput("v", type);
+		this.addProperty("value", defaultValue);
+		this.addWidget(widgetType, "value", defaultValue, "value");
+	}
+	node.title = "Constant (" + type + ")";
+	LiteGraph.registerNodeType(type + "/constant", node);
+}
+function registerBinaryNode(name, type, aDef, bDef, widgetType) {
+	function node() {
+		this.addOutput("v", type);
+		this.addInput("a", type);
+		this.addInput("b", type);
+
+		this.addProperty("a", aDef);
+		this.addProperty("b", bDef);
+
+		this.addWidget(widgetType, "a", aDef, "a");
+		this.addWidget(widgetType, "b", bDef, "b");
+	}
+	node.title = name[0].toUpperCase() + name.slice(1) + " (" + type + ")";
+	LiteGraph.registerNodeType(type + "/" + name, node);
+}
+
 // Numbers
 {
+	registerInputNode("number", "time");
+	// registerOutputNode("number", "");
+	registerConstantNode("number", 0, "number");
+
+	registerBinaryNode("add", "number", 0, 0, "number");
+	registerBinaryNode("sub", "number", 0, 0, "number");
+	registerBinaryNode("mult", "number", 0, 1, "number");
+	registerBinaryNode("div", "number", 0, 1, "number");
+
 	{
 		function node() {
 			this.addOutput("v", "number");
-			this.addProperty("name", "time");
-			this.widget = this.addWidget("text", "name", "time", "name");
+			this.addInput("a", "number");
+			this.addInput("b", "number");
+			this.addInput("t", "number");
+
+			this.addProperty("a", 0);
+			this.addProperty("b", 0);
+			this.addProperty("t", 0);
+
+			this.addWidget("number", "a", 0, "a");
+			this.addWidget("number", "b", 0, "b");
+			this.addWidget("number", "t", 0, "t");
 		}
-		node.title = "Number input";
-		LiteGraph.registerNodeType("number/input", node);
+		node.title = "Mix (number)";
+		LiteGraph.registerNodeType("number/mix", node);
 	}
 
 	{
@@ -15,7 +76,7 @@
 			this.addOutput("v", "number");
 			this.addInput("a", "number");
 		}
-		node.title = "Zigzag";
+		node.title = "Zig-zag";
 		node.desc = "Similar to modulo 1, but goes to v=1 at a=0.5 and back to v=0 at a=1 (and repeating)";
 		LiteGraph.registerNodeType("number/zigzag", node);
 	}
@@ -23,7 +84,7 @@
 	{
 		function node() {
 			this.addOutput("v", "number");
-			this.addInput("a", "number");
+			this.addInput("a", "number");			
 		}
 		node.title = "Smoothstep";
 		LiteGraph.registerNodeType("number/smoothstep", node);
@@ -32,25 +93,8 @@
 
 // Poses
 {
-	{
-		function node() {
-			this.addOutput("v", "pose");
-			this.addProperty("name", "pose:default");
-			this.widget = this.addWidget("text", "name", "pose:default", "name");
-		}
-		node.title = "Pose input";
-		LiteGraph.registerNodeType("pose/input", node);
-	}
-
-	{
-		function node() {
-			this.addInput("v", "pose");
-			this.addProperty("name", "pose");
-			this.widget = this.addWidget("text", "name", "pose", "name");
-		}
-		node.title = "Pose output";
-		LiteGraph.registerNodeType("pose/output", node);
-	}
+	registerInputNode("pose", "default");
+	registerOutputNode("pose", "pose");
 
 	{
 		function node() {
@@ -58,8 +102,12 @@
 			this.addInput("a", "pose");
 			this.addInput("b", "pose");
 			this.addInput("t", "number");
+
+			this.addProperty("t", 0);
+
+			this.addWidget("number", "t", 0, "t");
 		}
-		node.title = "Mix poses";
+		node.title = "Mix (pose)";
 		node.desc = "Linearly mixes two poses";
 		LiteGraph.registerNodeType("pose/mix", node);
 	}
