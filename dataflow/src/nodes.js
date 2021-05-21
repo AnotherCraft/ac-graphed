598bdaddea0e34585ab918942a0c9826cdf95ca3
Danol <czdanol@gmail.com>
2021-05-21 07:45:59 +0200

    no message

--- dataflow/src/nodes.js
+++ dataflow/src/nodes.js
@@ -25,13 +25,21 @@ function registerConstantNode(type, defaultValue, widgetType) {
 	node.title = "Constant (" + type + ")";
 	LiteGraph.registerNodeType(type + "/constant", node);
 }
+function registerUnaryNode(name, type) {
+	function node() {
+		this.addOutput("v", type);
+		this.addInput("a", type);
+	}
+	node.title = name[0].toUpperCase() + name.slice(1) + " (" + type + ")";
+	LiteGraph.registerNodeType(type + "/" + name, node);
+}
 function registerBinaryNode(name, type, aDef, bDef, widgetType = null) {
 	function node() {
 		this.addOutput("v", type);
 		this.addInput("a", type);
 		this.addInput("b", type);
 
-		if(widgetType) {
+		if (widgetType) {
 			this.addProperty("a", aDef);
 			this.addProperty("b", bDef);
 
@@ -42,34 +50,26 @@ function registerBinaryNode(name, type, aDef, bDef, widgetType = null) {
 	node.title = name[0].toUpperCase() + name.slice(1) + " (" + type + ")";
 	LiteGraph.registerNodeType(type + "/" + name, node);
 }
-function registerUnaryNode(name, type) {
-	function node() {
-		this.addOutput("v", type);
-		this.addInput("a", type);
-	}
-	node.title = name[0].toUpperCase() + name.slice(1) + " (" + type + ")";
-	LiteGraph.registerNodeType(type + "/" + name, node);
-}
 
-// number
+// num
 {
-	registerInputNode("number", "time");
-	// registerOutputNode("number", "");
-	registerConstantNode("number", 0, "number");
+	registerInputNode("num", "time");
+	registerOutputNode("num", "");
+	registerConstantNode("num", 0, "number");
 
-	registerBinaryNode("add", "number", 0, 0, "number");
-	registerBinaryNode("sub", "number", 0, 0, "number");
-	registerBinaryNode("mult", "number", 0, 1, "number");
-	registerBinaryNode("div", "number", 0, 1, "number");
+	registerBinaryNode("add", "num", 0, 0, "number");
+	registerBinaryNode("sub", "num", 0, 0, "number");
+	registerBinaryNode("mult", "num", 0, 1, "number");
+	registerBinaryNode("div", "num", 0, 1, "number");
 
-	registerUnaryNode("abs", "number");
+	registerUnaryNode("abs", "num");
 
 	{
 		function node() {
-			this.addOutput("v", "number");
-			this.addInput("a", "number");
-			this.addInput("b", "number");
-			this.addInput("t", "number");
+			this.addOutput("v", "num");
+			this.addInput("a", "num");
+			this.addInput("b", "num");
+			this.addInput("t", "num");
 
 			this.addProperty("a", 0);
 			this.addProperty("b", 0);
@@ -79,38 +79,41 @@ function registerUnaryNode(name, type) {
 			this.addWidget("number", "b", 0, "b");
 			this.addWidget("number", "t", 0, "t");
 		}
-		node.title = "Mix (number)";
-		LiteGraph.registerNodeType("number/mix", node);
+		node.title = "Mix (num)";
+		LiteGraph.registerNodeType("num/mix", node);
 	}
 
 	{
 		function node() {
-			this.addOutput("v", "number");
-			this.addInput("a", "number");
+			this.addOutput("v", "num");
+			this.addInput("a", "num");
 		}
 		node.title = "Zig-zag";
 		node.desc = "Similar to modulo 1, but goes to v=1 at a=0.5 and back to v=0 at a=1 (and repeating)";
-		LiteGraph.registerNodeType("number/zigzag", node);
+		LiteGraph.registerNodeType("num/zigzag", node);
 	}
 
 	{
 		function node() {
-			this.addOutput("v", "number");
-			this.addInput("a", "number");			
+			this.addOutput("v", "num");
+			this.addInput("a", "num");
 		}
 		node.title = "Smoothstep";
-		LiteGraph.registerNodeType("number/smoothstep", node);
+		LiteGraph.registerNodeType("num/smoothstep", node);
 	}
 }
 
-// vec3
+// vec
 {
+	registerInputNode("vec", "");
+	registerOutputNode("vec", "");
+
 	{
 		function node() {
-			this.addInput("x", "number");
-			this.addInput("y", "number");
-			this.addInput("z", "number");
-			this.addOutput("v", "vec3");
+			this.addInput("x", "num");
+			this.addInput("y", "num");
+			this.addInput("z", "num");
+			this.addOutput("v", "vec");
 
 			this.addProperty("x", 0);
 			this.addProperty("y", 0);
@@ -120,12 +123,83 @@ function registerUnaryNode(name, type) {
 			this.addWidget("number", "y", 0, "y");
 			this.addWidget("number", "z", 0, "z");
 		}
-		node.title = "Compose (vec3)";
-		LiteGraph.registerNodeType("vec3/compose", node);
+		node.title = "Compose (vec)";
+		LiteGraph.registerNodeType("vec/compose", node);
 	}
 
-	registerBinaryNode("add", "vec3", 0, 0);
-	registerBinaryNode("sub", "vec3", 0, 0);
+	registerBinaryNode("add", "vec", 0, 0);
+	registerBinaryNode("sub", "vec", 0, 0);
+
+	registerUnaryNode("normalize", "vec");
+}
+
+// qn
+{
+	registerInputNode("qn", "time");
+	registerOutputNode("qn", "");
+
+	{
+		function node() {
+			this.addInput("vec", "vec");
+			this.addInput("scal", "num");
+			this.addOutput("v", "qn");
+
+			this.addProperty("scal", 0);
+
+			this.addWidget("number", "scal", 0, "scal");
+		}
+		node.title = "Compose (qn)";
+		LiteGraph.registerNodeType("qn/compose", node);
+	}
+	{
+		function node() {
+			this.addInput("angles", "vec");
+			this.addOutput("v", "qn");
+		}
+		node.title = "From angles (qn)";
+		LiteGraph.registerNodeType("qn/fromEulerAngles", node);
+	}
+	{
+		function node() {
+			this.addInput("dir", "vec");
+			this.addInput("up", "vec");
+			this.addOutput("v", "qn");
+		}
+		node.title = "From dir (qn)";
+		LiteGraph.registerNodeType("qn/fromDirection", node);
+	}
+	{
+		function node() {
+			this.addInput("srcDir", "vec");
+			this.addInput("srcUp", "vec");
+			this.addInput("tgtDir", "vec");
+			this.addInput("tgtUp", "vec");
+			this.addOutput("v", "qn");
+		}
+		node.title = "From dir ext (qn)";
+		LiteGraph.registerNodeType("qn/fromDirectionExt", node);
+	}
+
+	registerBinaryNode("add", "qn");
+	registerBinaryNode("sub", "qn");
+	registerBinaryNode("mult", "qn");
+
+	{
+		function node() {
+			this.addOutput("v", "qn");
+			this.addInput("a", "qn");
+			this.addInput("b", "num");
+
+			this.addProperty("b", 0);
+
+			this.addWidget("number", "b", 0, "b");
+		}
+		node.title = "MultN (qn)";
+		LiteGraph.registerNodeType("qn/multn", node);
+	}
+
+	registerUnaryNode("normalize", "qn");
+	registerUnaryNode("invert", "qn");
 }
 
 // pose
@@ -138,7 +212,7 @@ function registerUnaryNode(name, type) {
 			this.addOutput("v", "pose");
 			this.addInput("a", "pose");
 			this.addInput("b", "pose");
-			this.addInput("t", "number");
+			this.addInput("t", "num");
 
 			this.addProperty("t", 0);
 
@@ -153,15 +227,16 @@ function registerUnaryNode(name, type) {
 		function node() {
 			this.addOutput("v", "pose");
 			this.addInput("pose", "pose");
-			this.addInput("offset", "vec3");
+			this.addInput("offset", "vec");
+			this.addInput("rotation", "qn");
 
 			this.addProperty("bone", "bone:");
 
 			this.addWidget("text", "bone", "bone:", "bone");
 		}
-		node.title = "Offet bone (pose)";
-		node.desc = "Adds offset to a given bone";
-		LiteGraph.registerNodeType("pose/offsetBone", node);
+		node.title = "Adjust bone (pose)";
+		node.desc = "Adds offset/rotation to a given bone";
+		LiteGraph.registerNodeType("pose/adjustBone", node);
 	}
 
 }
